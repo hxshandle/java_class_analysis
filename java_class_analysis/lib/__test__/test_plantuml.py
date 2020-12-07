fd921dc45329165119bb9f0f58764426b63891a6
Handle Home <handle.huang@outlook.com>
2020-12-07 23:10:43 +0800

    working

--- java_class_analysis/lib/__test__/test_plantuml.py
+++ java_class_analysis/lib/__test__/test_plantuml.py
@@ -1,7 +1,11 @@
 import shelve
-
+from lib import plantuml
+from assertpy import assert_that
 
 def test_sequence_diagram():
     db = shelve.open("test_db")
     ret = db['com.successfactors.gm.ui.fb.FBTGMBrowse']
     print('done')
+    klass = ret[0]
+    uml = plantuml.sequence_diagram(klass)
+    assert_that(uml).is_not_none()
--- java_class_analysis/lib/plantuml.py
+++ java_class_analysis/lib/plantuml.py
@@ -3,6 +3,14 @@ from lib.tree import JavaClassMeta
 
 def sequence_diagram(data: JavaClassMeta):
     # group out calls by out_calls
-
-    pass
+    out_methods = dict()
+    for target, invoked_method, from_method in data.out_calls:
+        line, column = from_method.position
+        key = "{}__{}__{}".format(from_method.name, line, column)
+        m = out_methods.get(key)
+        if not m:
+            m = []
+            out_methods[key] = m
+        m.append((target.split(".")[-1], target, invoked_method.member))
+    return "haha"
 
